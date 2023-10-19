0e2facf4e4812b69b955ea625f5b352c7aaa7939
Alexis Sánchez <aasanchez@gmail.com>
2023-10-19 21:14:25 +0200

    ✅ test: add unit tests for the isOddOrEven method
    This commit adds unit tests for the `isOddOrEven` method in the code. The tests check the expected status ("Odd" or "Even") of a given value.

--- evenorodd_test.go
+++ evenorodd_test.go
@@ -9,3 +9,17 @@ func Test_genSlice_checkSize(t *testing.T) {
 		t.Errorf("Expected slice lenght of %v, but go %v", value, len(slice))
 	}
 }
+
+func Test_isOddOrEven_checkOddStatus(t *testing.T) {
+	val := value(1)
+	if val.isOddOrEven() != "Odd" {
+		t.Errorf("Expected Odd")
+	}
+}
+
+func Test_isOddOrEven_checkEvenStatus(t *testing.T) {
+	val := value(3)
+	if val.isOddOrEven() != "Even" {
+		t.Errorf("Expected Even")
+	}
+}
